7b6efaedb354c8002f11b0d9a567b0df1847e491
datta <dattu99rockstar@gmail.com>
2024-08-21 11:22:56 +0530

    standy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import {MWComponent} from '../app/mw/mw.component'
 import { NgModule } from '@angular/core';
+
 export const routes: Routes = [
     { path: 'mw', component: MWComponent },
 ];
+RouterModule.forRoot(routes, { useHash: true })
+
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
   })
-  export class AppRoutingModule { }
+  
+export class AppRoutingModule { }
